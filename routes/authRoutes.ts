97c63d60e67a9b15c2c5eec67285fb89309ba581
Sohan Gamalath <gamalathsohan@gmail.com>
2025-04-23 13:27:58 +0530

    update:update code

--- routes/authRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { loginUser } from "../controllers/authController";
-
-const router = express.Router();
-
-// POST /api/auth/login - User login
-router.post("/login", loginUser);
-
-export default router;
--- server.ts
+++ server.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-
-import studentRoutes from "./routes/studentRoutes";
+import instituteRoutes from "./routes/instituteRoutes";
 import userRoutes from "./routes/userRoutes";
+import studentRoutes from "./routes/studentRoutes";
+import teachersRoutes from "./routes/teacherRoutes";
+import classRoutes from "./routes/classRoutes";
+import enrollmentRoutes from "./routes/enrollmentRoutes";
 import subjectRoutes from "./routes/subjectRoutes";
-import authRoutes from "./routes/authRoutes";
-
+import assignmentResultRoutes from "./routes/assignmentResultRoutes";
+import assignmentRoutes from "./routes/assignmentRoutes";
 dotenv.config();
 
 const app = express();
@@ -14,11 +17,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Routes
+app.use("/api/user", userRoutes);
+app.use("/api/institute", instituteRoutes);
 app.use("/api/students", studentRoutes);
-app.use("/api/users", userRoutes);
+app.use("/api/teachers", teachersRoutes);
+app.use("/api/classes", classRoutes);
+app.use("/api/enrollments", enrollmentRoutes);
 app.use("/api/subjects", subjectRoutes);
-app.use("/api/auth", authRoutes);
-
+app.use("/api/assignmentResults", assignmentResultRoutes);
+app.use("/api/assignments", assignmentRoutes);
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
